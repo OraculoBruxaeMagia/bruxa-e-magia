88c429b2dcfdd9cd09349d9a52b2f3feb915ff5d
Oraculo bruxa e magia <elizz1966@gmail.com>
2026-08-16 16:26:26 -0300

    Update horoscopo.js

--- netlify/functions/horoscopo.js
+++ netlify/functions/horoscopo.js
@@ -1,19 +1,16 @@
-const axios = require('axios'); // ou use fetch se preferir
+const axios = require('axios');
 
 exports.handler = async (event, context) => {
-  // Pega o signo enviado pelo front-end (ex: ?signo=aquarius)
-  const signo = event.queryStringParameters.sign), || 'aquarius';
-
-  const options = {
+  const signo = event.queryStringParameters.sign || 'aquarius';
+   const options = {
     method: 'GET',
     url: 'https://rapidapi.com',
     params: {
       zodiac: signo,
-      language: 'pt', // Força o idioma para português
+      language: 'pt',
       type: 'daily'
     },
     headers: {
-      // Puxa AUTOMATICAMENTE os valores que você salvou no painel do Netlify
       'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
       'X-RapidAPI-Host': process.env.RAPIDAPI_HOST
     }
@@ -23,12 +20,21 @@ exports.handler = async (event, context) => {
     const response = await axios.request(options);
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(response.data)
     };
   } catch (error) {
     return {
       statusCode: error.response ? error.response.status : 500,
-      body: JSON.stringify({ error: "Erro na API do Horóscopo", detalhe: error.message })
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ error: "A API de horóscopo respondeu com erro.", detalhe: error.message })
     };
   }
 };
