8a0b77a44d0dd13a417cd94bfc8ab45a4cde2e69
Oraculo bruxa e magia <elizz1966@gmail.com>
2026-08-18 17:00:25 -0300

    Update horoscopo.js

--- netlify/functions/horoscopo.js
+++ netlify/functions/horoscopo.js
@@ -1,52 +1,42 @@
 const https = require('https');
 
 exports.handler = async (event, context) => {
-  const signo = (event.queryStringParameters && event.queryStringParameters.sign) || 'aquarius';
-  
-  // URL correta fornecida pela RapidAPI
-  const host = process.env.RAPIDAPI_HOST || 'astropredict-daily-horoscopes-lucky-insights.p.rapidapi.com';
-  const urlCompleta = `https://${host}/horoscope?lang=pt&zodiac=${signo}&type=daily&timezone=UTC`;
+    const signo = event.queryStringParameters.signo || 'aries';
+    
+    const host = process.env.RAPIDAPI_HOST || 'astropredict-data.p.rapidapi.com';
+    const urlCompleta = `https://${host}/horoscope?zodiac=${signo}&type=daily`;
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': process.env.RAPIDAPI_KEY,
-      'x-rapidapi-host': host
-    }
-  };
-
-  return new Promise((resolve) => {
-    const req = https.request(urlCompleta, options, (res) => {
-      let data = '';
-
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      res.on('end', () => {
-        resolve({
-          statusCode: res.statusCode,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Content-Type": "application/json"
-          },
-          body: data
-        });
-      });
-    });
-
-    req.on('error', (error) => {
-      resolve({
-        statusCode: 500,
+    const options = {
+        method: 'GET',
         headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ error: "Falha ao carregar os dados.", detalhe: error.message })
-      });
-    });
+            'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+            'x-rapidapi-host': host
+        }
+    };
 
-    req.end();
-  });
+    return new Promise((resolve, reject) => {
+        const req = https.request(urlCompleta, options, (res) => {
+            let dados = '';
+            res.on('data', (chunk) => dados += chunk);
+            res.on('end', () => {
+                resolve({
+                    statusCode: res.statusCode,
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Content-Type": "application/json"
+                    },
+                    body: dados
+                });
+            });
+        });
+
+        req.on('error', (erro) => {
+            reject({
+                statusCode: 500,
+                body: JSON.stringify({ error: erro.message })
+            });
+        });
+
+        req.end();
+    });
 };
